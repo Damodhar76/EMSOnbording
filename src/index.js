8fd66cf34c34556bfba857b1f81079f37b8ace10
Balaram2153 <balubalaramireddyk@gmail.com>
2024-06-07 23:21:02 +0530

    dash board and emp account creation integrated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import AddCandidets from './HR/AddCandidets';
+import Orientation from './HR/Orientation';
+import HrDashbord from './HR/HrDashbord';
+import EmpDashboard from './EMP/EmpDashboard';
+import OnBordingList from './HR/OnBordingList';
+import Basicinfo from './EMP/Basicinfo';
+import Declaresubmit from './EMP/Declare&submit';
+import Documentsubmission from './EMP/Documentsubmission';
+import NewEmployee from './EMP/NewEmployee';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+       
+       <Routes>
+     
+        <Route path='/AddCandidets' element={<AddCandidets/>}/>
+        <Route path='/' element={<HrDashbord/>}/>
+        <Route path='/Orientation' element={<Orientation/>}/>
+        <Route path='/EmpDashboard' element={<EmpDashboard/>}/>
+        <Route path='/OnBordingList' element={<OnBordingList/>}/>
+        <Route path='/Basicinfo' element={<Basicinfo/>}></Route>
+        <Route path='/Declaresubmit' element={<Declaresubmit/>}></Route>
+        <Route path='/Documentsubmission' element={<Documentsubmission/>}></Route>
+        <Route path='/NewEmployee' element={<NewEmployee/>}></Route>
+        
+        
+
+       </Routes>
+      
+    </BrowserRouter>
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
